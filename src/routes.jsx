eef4ac9ebafe74854ca362f19ee99d86da5b92e1
Guilherme Moura <killecarter1473@hotmail.com>
2023-08-12 03:55:06 -0300

    teste

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,26 +1,26 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import React, { useContext, useEffect } from 'react'
-import './styles/estilosGlobais.css'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import React, { useContext, useEffect } from "react";
+import "./styles/estilosGlobais.css";
 
-import Posologia from './paginas/Posologia'
-import Inicio from './paginas/Inicio'
-import Login from './paginas/Login'
-import DefaultPageLayout from './DefaultPageLayout'
-import { AuthProvider, AuthContext } from './contexts/AuthContext'
-import Servicos from './paginas/Servicos'
-import Farmapop from './paginas/Farmapop'
-import Tutorial from './paginas/Tutorial'
-import Encomendas from './paginas/Encomendas'
+import Posologia from "./paginas/Posologia";
+import Inicio from "./paginas/Inicio";
+import Login from "./paginas/Login";
+import DefaultPageLayout from "./DefaultPageLayout";
+import { AuthProvider, AuthContext } from "./contexts/AuthContext";
+import Servicos from "./paginas/Servicos";
+import Farmapop from "./paginas/Farmapop";
+import Tutorial from "./paginas/Tutorial";
+import Encomendas from "./paginas/Encomendas";
 
 const AppRoutes = () => {
   const Private = ({ children }) => {
     const { authenticated, loading } = useContext(AuthContext);
-    
+
     if (loading) {
       return <div className="loading">Carregando</div>;
     }
 
-    if(!authenticated) {
+    if (!authenticated) {
       return <Navigate to="/login" />;
     }
 
@@ -31,38 +31,71 @@ const AppRoutes = () => {
     <BrowserRouter>
       <AuthProvider>
         <Routes>
+          <Route path="/login" element={<Login />} />
           <Route
-            path='/login'
-            element={<Login />}
+            path="/*"
+            element={
+              <Private>
+                <DefaultPageLayout>
+                  <Inicio />
+                </DefaultPageLayout>
+              </Private>
+            }
           />
           <Route
-            index
-            element={<Private><DefaultPageLayout><Inicio /></DefaultPageLayout></Private>}
+            path="/posologia"
+            element={
+              <Private>
+                <DefaultPageLayout>
+                  <Posologia />
+                </DefaultPageLayout>
+              </Private>
+            }
           />
           <Route
-            path='/posologia'
-            element={<Private><DefaultPageLayout><Posologia /></DefaultPageLayout></Private>}
+            path="/servicos"
+            element={
+              <Private>
+                <DefaultPageLayout>
+                  <Servicos />
+                </DefaultPageLayout>
+              </Private>
+            }
           />
           <Route
-            path='/servicos'
-            element={<Private><DefaultPageLayout><Servicos/></DefaultPageLayout></Private>}
+            path="/encomendas"
+            element={
+              <Private>
+                <DefaultPageLayout>
+                  <Encomendas />
+                </DefaultPageLayout>
+              </Private>
+            }
           />
           <Route
-            path='/encomendas'
-            element={<Private><DefaultPageLayout><Encomendas/></DefaultPageLayout></Private>}
+            path="/farmapop"
+            element={
+              <Private>
+                <DefaultPageLayout>
+                  <Farmapop />
+                </DefaultPageLayout>
+              </Private>
+            }
           />
           <Route
-            path='/farmapop'
-            element={<Private><DefaultPageLayout><Farmapop/></DefaultPageLayout></Private>}
-          />
-          <Route
-            path='/tutorial'
-            element={<Private><DefaultPageLayout><Tutorial/></DefaultPageLayout></Private>}
+            path="/tutorial"
+            element={
+              <Private>
+                <DefaultPageLayout>
+                  <Tutorial />
+                </DefaultPageLayout>
+              </Private>
+            }
           />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
